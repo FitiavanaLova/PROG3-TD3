8b229a172b1ff7891e3ff4f3f784d3e45561b6df
FitiavanaLova <hei.fitiavana@gmail.com>
2026-01-19 09:18:48 +0300

    dish

---
+++ target/classes/sql/db.sql
@@ -0,0 +1,12 @@
+create database "mini_dish_db";
+
+create user "mini_dish_db_manager" with password '123456';
+
+-- Grant all privileges
+
+GRANT ALL PRIVILEGES ON DATABASE mini_dish_db TO mini_dish_db_manager;
+GRANT ALL ON SCHEMA public TO mini_dish_db_manager;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mini_dish_db_manager;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mini_dish_db_manager;
+
+psql -U mini_dish_db_manager -d mini_dish_db
